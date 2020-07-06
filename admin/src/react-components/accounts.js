9839aaa85ff18ab7d7d125daf7c5b8a9597736af
robin <robin.kath.wilson@gmail.com>
2020-07-06 13:56:36 -0700

    pass new toolbar into edit

--- admin/src/react-components/accounts.js
+++ admin/src/react-components/accounts.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { IdentityEditLink, IdentityCreateLink } from "./fields";
-import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import MuiTextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Snackbar from "@material-ui/core/Snackbar";
-import SnackbarContent from "@material-ui/core/SnackbarContent";
+import React, { Component } from 'react'
+import { connect, SaveButton } from 'react-redux'
+import { IdentityEditLink, IdentityCreateLink } from './fields'
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import MuiTextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Snackbar from '@material-ui/core/Snackbar'
+import SnackbarContent from '@material-ui/core/SnackbarContent'
 
 import {
   BooleanField,
@@ -26,19 +26,19 @@ import {
   TextField,
   TextInput,
   refreshView
-} from "react-admin";
+} from 'react-admin'
 
 const styles = {
-  hide: { display: "none" },
-  noBorder: { border: "0px" },
-  searchCard: { marginBottom: "5px" }
-};
+  hide: { display: 'none' },
+  noBorder: { border: '0px' },
+  searchCard: { marginBottom: '5px' }
+}
 
 const AccountFilter = props => (
   <Filter {...props}>
     <TextInput label="Search ID" source="_text_id" alwaysOn />
   </Filter>
-);
+)
 
 export const AccountList = withStyles(styles)(
   connect(
@@ -47,63 +47,63 @@ export const AccountList = withStyles(styles)(
   )(
     class AccountList extends Component {
       state = {
-        emailSearch: "",
+        emailSearch: '',
         searching: false,
         searchStatus: null,
-        batchCreate: "",
+        batchCreate: '',
         creating: false,
         createStatus: null,
-        createResults: ""
-      };
+        createResults: ''
+      }
       componentWillUnmount() {
-        this.clearCreateStatusTimer();
-        this.clearSearchStatusTimer();
+        this.clearCreateStatusTimer()
+        this.clearSearchStatusTimer()
       }
       clearCreateStatusTimer() {
         if (this.createStatusTimer) {
-          clearTimeout(this.createStatusTimer);
-          this.createStatusTimer = null;
+          clearTimeout(this.createStatusTimer)
+          this.createStatusTimer = null
         }
       }
       clearSearchStatusTimer() {
         if (this.searchStatusTimer) {
-          clearTimeout(this.searchStatusTimer);
-          this.searchStatusTimer = null;
+          clearTimeout(this.searchStatusTimer)
+          this.searchStatusTimer = null
         }
       }
       async onAccountSearch(e) {
-        e.preventDefault();
-        this.setState({ searching: true, searchStatus: null });
-        const result = await fetch("/api/v1/accounts/search", {
-          method: "post",
+        e.preventDefault()
+        this.setState({ searching: true, searchStatus: null })
+        const result = await fetch('/api/v1/accounts/search', {
+          method: 'post',
           headers: {
-            "content-type": "application/json",
+            'content-type': 'application/json',
             authorization: `bearer ${window.APP.store.state.credentials.token}`
           },
-          body: JSON.stringify({ email: this.state.emailSearch || "" })
-        }).then(r => r.json());
+          body: JSON.stringify({ email: this.state.emailSearch || '' })
+        }).then(r => r.json())
         if (result && result.data) {
-          window.location = `#/accounts/${result.data[0].id}`;
+          window.location = `#/accounts/${result.data[0].id}`
         } else {
-          this.setState({ searching: false, searchStatus: "Account not found" });
+          this.setState({ searching: false, searchStatus: 'Account not found' })
         }
         // Quickfix snackbar component does not always close
         // Setting snackbar message to empty string closes
-        this.clearSearchStatusTimer();
+        this.clearSearchStatusTimer()
         this.searchStatusTimer = setTimeout(() => {
-          this.setState({ searchStatus: "" });
-          this.searchStatusTimer = null;
-        }, 6000);
+          this.setState({ searchStatus: '' })
+          this.searchStatusTimer = null
+        }, 6000)
       }
       async onCreateAccount(e) {
-        e.preventDefault();
-        if (this.state.batchCreate.length === 0) return;
-        this.setState({ creating: true, createStatus: null });
+        e.preventDefault()
+        if (this.state.batchCreate.length === 0) return
+        this.setState({ creating: true, createStatus: null })
         const data = this.state.batchCreate
-          .split(";") // ['email1,identity1', '', 'email2','email3,identity with spaces', 'email4']
-          .filter(accounts => accounts !== "")
+          .split(';') // ['email1,identity1', '', 'email2','email3,identity with spaces', 'email4']
+          .filter(accounts => accounts !== '')
           .map(accounts => {
-            const emailAndIdentity = accounts.split(",");
+            const emailAndIdentity = accounts.split(',')
             return emailAndIdentity.length === 1
               ? {
                   email: emailAndIdentity[0].trim()
@@ -111,24 +111,24 @@ export const AccountList = withStyles(styles)(
               : {
                   email: emailAndIdentity[0].trim(),
                   name: emailAndIdentity[1].trim()
-                };
-          });
-        const result = await fetch("/api/v1/accounts", {
-          method: "post",
+                }
+          })
+        const result = await fetch('/api/v1/accounts', {
+          method: 'post',
           headers: {
-            "content-type": "application/json",
+            'content-type': 'application/json',
             authorization: `bearer ${window.APP.store.state.credentials.token}`
           },
           body: JSON.stringify({
             data: data.length === 1 ? data[0] : data
           })
-        }).then(r => r.json());
+        }).then(r => r.json())
         if (result && result.data) {
           // one email added successfully
-          this.setState({ creating: false, createStatus: `Account created successfully` });
+          this.setState({ creating: false, createStatus: `Account created successfully` })
         } else if (result && result.errors) {
           // one email has errors
-          this.setState({ creating: false, createStatus: result.errors[0].detail });
+          this.setState({ creating: false, createStatus: result.errors[0].detail })
         } else if (Array.isArray(result)) {
           // Multiple email accounts created
           // results = {
@@ -136,39 +136,39 @@ export const AccountList = withStyles(styles)(
           //   'errorMsg1': [email4],
           //   'errorMsg2': [email5, email6]
           // }
-          const results = {};
-          let isAllSuccess = true;
-          let hasOneSuccess = false;
+          const results = {}
+          let isAllSuccess = true
+          let hasOneSuccess = false
           result.forEach((emailResponse, index) => {
-            isAllSuccess = isAllSuccess && emailResponse.status === 200;
-            hasOneSuccess = hasOneSuccess || emailResponse.status === 200;
+            isAllSuccess = isAllSuccess && emailResponse.status === 200
+            hasOneSuccess = hasOneSuccess || emailResponse.status === 200
             const message =
-              emailResponse.status === 200 ? "Created accounts successfully" : emailResponse.body.errors[0].detail;
-            const email = data[index].email;
-            if (results[message]) results[message].push(email);
-            else results[message] = [email];
-          });
+              emailResponse.status === 200 ? 'Created accounts successfully' : emailResponse.body.errors[0].detail
+            const email = data[index].email
+            if (results[message]) results[message].push(email)
+            else results[message] = [email]
+          })
           this.setState({
             creating: false,
             createStatus: isAllSuccess
-              ? "Success adding all accounts"
+              ? 'Success adding all accounts'
               : hasOneSuccess
-              ? "Success adding some accounts, Errors adding some accounts"
-              : "Errors adding all accounts",
+              ? 'Success adding some accounts, Errors adding some accounts'
+              : 'Errors adding all accounts',
             createResults: results
-          });
+          })
         }
-        this.props.refreshView();
+        this.props.refreshView()
         // Quickfix snackbar component does not always close
         // Setting snackbar message to empty string closes
-        this.clearCreateStatusTimer();
+        this.clearCreateStatusTimer()
         this.createStatusTimer = setTimeout(() => {
-          this.setState({ createStatus: "" });
-          this.createStatusTimer = null;
-        }, 6000);
+          this.setState({ createStatus: '' })
+          this.createStatusTimer = null
+        }, 6000)
       }
       render() {
-        const { classes } = this.props;
+        const { classes } = this.props
         return (
           <>
             <Card classes={{ root: classes.searchCard }}>
@@ -186,7 +186,7 @@ export const AccountList = withStyles(styles)(
                   <MuiTextField
                     label="Email, (optional) identity"
                     type="text"
-                    style={{ minWidth: "300px" }}
+                    style={{ minWidth: '300px' }}
                     required
                     onChange={e => this.setState({ batchCreate: e.target.value })}
                   />
@@ -201,13 +201,13 @@ export const AccountList = withStyles(styles)(
                     <>
                       <Typography
                         component="p"
-                        color={message.includes("success") ? "textPrimary" : "error"}
-                        style={{ paddingTop: "10px" }}
+                        color={message.includes('success') ? 'textPrimary' : 'error'}
+                        style={{ paddingTop: '10px' }}
                       >
                         {message}
                       </Typography>
-                      <Typography component="p" color="secondary" style={{ paddingBottom: "10px" }}>
-                        [ {this.state.createResults[message].join(", ")} ]
+                      <Typography component="p" color="secondary" style={{ paddingBottom: '10px' }}>
+                        [ {this.state.createResults[message].join(', ')} ]
                       </Typography>
                     </>
                   ))}
@@ -250,23 +250,29 @@ export const AccountList = withStyles(styles)(
               </Datagrid>
             </List>
           </>
-        );
+        )
       }
     }
   )
-);
+)
+
+const PostEditToolbar = props => (
+  <Toolbar {...props}>
+    <SaveButton />
+  </Toolbar>
+)
 
 export const AccountEdit = withStyles(styles)(props => {
-  const { classes } = props;
+  const { classes } = props
 
   return (
     <Edit {...props}>
-      <SimpleForm>
+      <SimpleForm toolbar={<PostEditToolbar />}>
         <TextField label="Account ID" source="id" />
         <BooleanInput source="is_admin" />
         <SelectInput
           source="state"
-          choices={[{ id: "enabled", name: "enabled" }, { id: "disabled", name: "disabled" }]}
+          choices={[{ id: 'enabled', name: 'enabled' }, { id: 'disabled', name: 'disabled' }]}
         />
 
         <ReferenceManyField label="Identity" target="_account_id" reference="identities">
@@ -277,5 +283,5 @@ export const AccountEdit = withStyles(styles)(props => {
         </ReferenceManyField>
       </SimpleForm>
     </Edit>
-  );
-});
+  )
+})
