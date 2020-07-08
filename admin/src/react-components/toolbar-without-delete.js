0d1462e48e7197d506c303e7b798ea90cb66a63d
robin <robin.kath.wilson@gmail.com>
2020-07-07 19:09:57 -0700

    solved mysterious material ui error

--- admin/src/react-components/toolbar-without-delete.js
+++ admin/src/react-components/toolbar-without-delete.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import { Toolbar, SaveButton } from 'react-admin'
+import React from "react";
+import { Toolbar, SaveButton } from "react-admin";
 
 export const ToolbarWithoutDelete = props => {
-  console.log(props)
   return (
     <Toolbar {...props}>
       <SaveButton />
     </Toolbar>
-  )
-}
+  );
+};
